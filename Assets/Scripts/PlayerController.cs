4a484a9c2485d5b7fd7e30f3f1acb5c1a6e0badc
mayawingardh <mayaandersson@hotmail.se>
2021-11-23 15:25:39 +0100

    animation all direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -60,8 +60,8 @@ public class PlayerController : MonoBehaviour
         }
 
         animator.SetFloat("AnimHorizontal",playerInput.x);
-        
 
+        animator.SetFloat("AnimVertical", playerInput.y);
 
 
 
