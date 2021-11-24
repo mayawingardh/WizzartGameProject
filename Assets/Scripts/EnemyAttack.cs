c3638d61ba036f4989ffa9132de350b51321ce98
Marcus Rundén <marcus.runden@hotmail.com>
2021-11-24 11:03:27 +0100

    Lollipop attack added

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
+    private Transform player;
+    public GameObject lollipop;
+
+    private float timeBtwShots;
+    public float fireRate; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeBtwShots = fireRate;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Governs how long it takes for the enemy to attack again
+        if (timeBtwShots <= 0)
+        {
+            Instantiate(lollipop, transform.position, Quaternion.identity);
+            timeBtwShots = fireRate;
+        }
+        else
+        {
+            timeBtwShots -= Time.deltaTime;
+        }
         
     }
 }
---
+++ Assets/Scripts/Lollipop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lollipop : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+
+    private void Start()
+    {
+        //Makes lollipop find the position of the Player
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+
+    }
+
+    private void Update()
+    {
+        //Makes lollipop move towards players position and destroys lollipop when it hits Players position
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyLollipop();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //Destroys lollipop when it hits player
+        if (other.CompareTag("Player"))
+        {
+            DestroyLollipop();
+        }
+    }
+    void DestroyLollipop()
+    {
+        Destroy(gameObject);
+    }
+}
