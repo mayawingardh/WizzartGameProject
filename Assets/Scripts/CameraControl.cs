51b1a9510fd4a56d1d98b85ef636b8a752d67d20
Marcus Rundén <marcus.runden@hotmail.com>
2021-11-29 16:03:09 +0100

    Attempt at camera changes

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,11 +7,18 @@ public class CameraControl : MonoBehaviour
     public GameObject target;
     public Camera Cam1;
     public Camera Cam2;
+    public Transform leftBound;
+    public Transform rightBound;
+    public Transform topBound;
+    public Transform bottomBound;
+
+    Camera gameCamera;
 
     Vector3 offset;
-
+    Vector3 targetPos;
     void Start()
     {
+        gameCamera = Camera.main;
         Cam1.enabled = true;
         Cam2.enabled = false;
         offset = transform.position - target.transform.position;
@@ -26,5 +33,20 @@ public class CameraControl : MonoBehaviour
             Cam1.enabled = !Cam1.enabled;
             Cam2.enabled = !Cam2.enabled;
         }
+    }
+    private void LateUpdate()
+    {
+        //Vector3 tmpPosition = transform.position;
+        targetPos = transform.position;
+        float minX = leftBound.position.x + gameCamera.orthographicSize * gameCamera.aspect;
+        float maxX = rightBound.position.x - gameCamera.orthographicSize * gameCamera.aspect;
+
+        //tmpPosition.x = Mathf.Clamp(tmpPosition.x, minX, maxX);
+        targetPos.x = Mathf.Clamp(targetPos.x, minX, maxX);
+        float maxY = topBound.position.y - gameCamera.orthographicSize;
+        float minY = bottomBound.position.y + gameCamera.orthographicSize;
+        //tmpPosition.y = Mathf.Clamp(tmpPosition.y, minY, maxY);
+        targetPos.y = Mathf.Clamp(targetPos.y, minY, maxY);
+        //transform.position = tmpPosition;
     }
 }
