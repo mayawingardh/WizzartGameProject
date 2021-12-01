ca18f6e5c66cb9e98b3fceecc32165b1f467c822
mayawingardh <mayaandersson@hotmail.se>
2021-12-01 15:26:44 +0100

    Lagt in musik

---
+++ Assets/Scripts/PickUpAmmo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpAmmo : MonoBehaviour
+{
+    GameObject gun;
+    Gun refGun;
+    int ammoPU = 10;
+
+    private void Start()
+    {
+        gun = GameObject.FindGameObjectWithTag("Gun");
+        refGun = gun.GetComponent<Gun>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            refGun.RefillAmmo(ammoPU);
+            Destroy(gameObject);
+        }
+    }
+}
