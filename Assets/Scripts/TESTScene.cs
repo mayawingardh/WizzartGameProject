983939bca5ca5e732113ed86541a64262b3b9fdd
Codatron <87000871+Codatron@users.noreply.github.com>
2021-12-02 09:44:19 +0100

    Revert "Revert "Ateljen changes""
    This reverts commit 5a92ae5eee29dc4c29c1bddb83f1b25073e83a0e.

---
+++ Assets/Scripts/TESTScene.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TESTScene : MonoBehaviour
+{
+    GameObject panelDialoguePopArt;
+    GameObject panelDialogueSurreal;
+
+
+    private void Start()
+    {
+        panelDialoguePopArt = GameObject.FindGameObjectWithTag("PanelDialoguePopArt");
+        panelDialoguePopArt.SetActive(false);
+
+        panelDialogueSurreal = GameObject.FindGameObjectWithTag("PanelDialogueSurreal");
+        panelDialogueSurreal.SetActive(false);
+    }
+
+    // Prompts for Pop Art
+    public void DialoguePopArt()
+    {
+        panelDialoguePopArt.SetActive(true);
+    }
+
+    public void LoadRoomPopArt()
+    {
+        SceneManager.LoadScene("SceneRoomPopArt");
+    }
+
+    public void CancelRoomPopArt()
+    {
+        panelDialoguePopArt.SetActive(false);
+    }
+
+    // Prompts for Surreal
+    public void DialogueSurreal()
+    {
+        panelDialogueSurreal.SetActive(true);
+    }
+
+    public void LoadRoomSurreal()
+    {
+        SceneManager.LoadScene("SceneRoomSurreal");
+    }
+
+    public void CancelRoomSurreal()
+    {
+        panelDialogueSurreal.SetActive(false);
+    }
+
+
+}
+
+//public int index;
+//public int indexMax;
+//[SerializeField] bool keyDown;
+
+//void Update()
+//{
+//    int choice = (int)Input.GetAxis("Horizontal");
+
+//    if (choice != 0)
+//    {
+//        if (!keyDown)
+//        {
+//            if (choice < 0)
+//            {
+//                if (index < indexMax)
+//                {
+//                    index++;
+//                }
+//                else
+//                {
+//                    index = 0;
+//                }
+//            }
+//            else if (choice > 0)
+//            {
+//                if (index > 0)
+//                {
+//                    index--;
+//                }
+//                else
+//                {
+//                    index = indexMax;
+//                }
+//            }
+
+//            keyDown = true;
+//        }
+//    }
+//    else
+//    {
+//        keyDown = false;
+//    }
+//}
